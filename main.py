d042a9a7df4b4cbfeb0ade9641d8335ec2951766
Dexter <Coder0xH@gmail.com>
2025-05-31 15:31:04 +0800

    update

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=80,
+        port=8000,
         reload=True,
         log_level="info"
     )
